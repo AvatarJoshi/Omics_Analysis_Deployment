b3a14b5314b6e7b1711cdd4ca1c4b84a286bfec5
Avatar Joshi <joshi.avatar@gmail.com>
2023-03-01 15:15:06 -0800

    Loaded mock proteomics dataset into R.

--- omics_analysis.r
+++ omics_analysis.r
@@ -1,6 +1,8 @@
- # Install dependences
-install.packages("readxl")
  # Import dependences
  library("readxl")
+ library(tidyverse)
+ library(dplyr)
+ 
  # Read in the data
-my_data <- read_excel("pgen.1008897.s004.xlsx") 
+my_data <- read_excel("~/Desktop/DataLand/r5_Omics_Analysis_Deployment/pgen.1008897.s004.xlsx") 
+my_data
